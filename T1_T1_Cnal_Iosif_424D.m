1ca5ad5ddafbabdfad7b191a1eb2ea98bb9bed8b
CnalIosif <57264440+CnalIosif@users.noreply.github.com>
2019-11-01 18:51:39 +0200

    Update T1_T1_Cnal_Iosif_424D.m

--- T1_T1_Cnal_Iosif_424D.m
+++ T1_T1_Cnal_Iosif_424D.m
@@ -5,7 +5,7 @@ t=0:0.002:2; %timpul de la 0 la 2 cu pasul de 2 ms
 x=0.75*square(2*pi*0.5*t, 25)-0.25; % functia 'square' va afisa un semnal dreptunghiular cu factorul de umplere care va avea valoarea 0.25
 %amplitudinea are valoarea 0.75
 %componenta continua are valoarea -0.25
-plot(t,x,'_-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %afisarea graficului x=f(t)
+plot(t,x,'-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %afisarea graficului x=f(t)
 
 %Semnal dreptunghiular cu rezolutie temporara de 20ms 
 
@@ -14,7 +14,7 @@ t=0:0.02:2; %timpul de la 0 la 2 cu pasul de 20 ms
 x=0.75*square(2*pi*0.5*t, 25)-0.25; % functia 'square' va afisa un semnal dreptunghiular cu factorul de umplere cu valoarea de 0.25
 %amplitudinea are valoarea 0.75
 %componenta continua are valoarea -0.25
-plot(t,x,'_-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %se afiseaza graficul x=f(t)
+plot(t,x,'-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %se afiseaza graficul x=f(t)
 
 %Semnal dreptunghiular cu rezolutie temporara de 200ms 
 
@@ -23,4 +23,4 @@ t=0:0.2:2; %timpul de la 0 la 2 cu pasul de 200 ms
 x=0.75*square(2*pi*0.5*t, 25)-0.25; % functia 'square' va afisa un semnal dreptunghiular cu factorul de umplere cu valoarea de 0.25
 %amplitudinea are valoarea 0.75
 %componenta continua are valoarea -0.25
-plot(t,x,'_-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %se afiseaza graficul x=f(t)
+plot(t,x,'-'),xlabel('Timp[s]'),ylabel('Amplitudine'),title('Semnal dreptunghiular'),grid; %se afiseaza graficul x=f(t)
