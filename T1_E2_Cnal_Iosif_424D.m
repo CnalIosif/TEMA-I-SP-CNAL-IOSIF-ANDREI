e88d6304f5b6731ff571dc25504e94a9a7515278
CnalIosif <57264440+CnalIosif@users.noreply.github.com>
2019-11-01 18:25:54 +0200

    Update T1_E2_Cnal_Iosif_424D.m

--- T1_E2_Cnal_Iosif_424D.m
+++ T1_E2_Cnal_Iosif_424D.m
@@ -1,7 +1,7 @@
-a=randn(4) %se genereaza vector de dimensiune 4
-for i=1:1:16 %se parcurge fiecare element din vector pornind de la primul si ajungand la ultimul
-    if a(i) < 0 %daca este gasit un element mai mic decat 0
-        a(i) %se afiseaza elementul respectiv
+a=randn(1.4) %se genereaza vector de dimensiune 4
+for i=1:1:4 %se parcurge fiecare element din vector pornind de la primul si ajungand la ultimul
+    if a(i) < 0 %se afiseaza elementele negative
+        a(i)
         
     end  %se inchide if-ul
 end %se inchide for-ul
