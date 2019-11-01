784898a6c9d7aa8afe54999692ccbf9182483b2a
CnalIosif <57264440+CnalIosif@users.noreply.github.com>
2019-11-01 18:45:00 +0200

    Update T1_T4_Cnal_Iosif_424D.m

--- T1_T4_Cnal_Iosif_424D.m
+++ T1_T4_Cnal_Iosif_424D.m
@@ -6,7 +6,8 @@ t1 = 0:0.002:1.5; % timpul de la 0 la 1.5 secunde cu rezolutie temporara de 2ms
 t2=1.5:0.002:3 ; %timpul de la 1.5 la 3 secunde care are amplitudinea 0
 s1 = A*sin(2*pi*F*t1);     %  sin va desena un grafic sinusoidal cu amplitudinea A, perioada t si frecventa F
 s2=0*t2;
-plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
+figure
+plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
 
 %Semnal sinusoidal RMA  cu rezolutie temporara 20ms
 A = 0.8;   % amplitudinea
@@ -16,7 +17,8 @@ t1 = 0:0.02:1.5; % timpul de la 0 la 1.5 secunde cu rezolutie temporara de 2ms
 t2=1.5:0.02:3 ; %timpul de la 1.5 la 3 secunde care are amplitudinea 0
 s1 = A*sin(2*pi*F*t1);     %  sin va desena un grafic sinusoidal cu amplitudinea A, perioada t si frecventa F
 s2=0*t2;
-plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
+figure
+plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
 
 %Semnal sinusoidal RMA  cu rezolutie temporara 200ms
 A = 0.8;   % amplitudinea
@@ -26,5 +28,6 @@ t1 = 0:0.2:1.5; % timpul de la 0 la 1.5 secunde cu rezolutie temporara de 2ms
 t2=1.5:0.2:3 ; %timpul de la 1.5 la 3 secunde care are amplitudinea 0
 s1 = A*sin(2*pi*F*t1);     %  sin va desena un grafic sinusoidal cu amplitudinea A, perioada t si frecventa F
 s2=0*t2;
-plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]')],ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
+figure
+plot([t1,t2],[s1,s2],'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('Semnal sinusoidal'),grid; %se reprezinta grafic semnalul
 
